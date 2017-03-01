ca31ccdfdce5fafa8e30d3ec93e5104a172910e4
Yulin Zhu <zieft@hotmail.com>
2017-03-01 20:58:14 +0100

    add application 1.5

---
+++ Applications/1.5_Student_Report/studentreport.py
@@ -0,0 +1,47 @@
+from studentfile import StudentFileReader
+
+FILE_NAME = "students.txt"
+
+
+def main():
+    """
+    Extract the student records from the given text file.
+    :return:
+    """
+    reader = StudentFileReader(FILE_NAME)
+    reader.open()
+    studentList = reader.fetchAll()
+    reader.close()
+
+    # Sort the list by id number. Each object is passed to the lambda
+    # expression which returns the idNum field of the object.
+    studentList.sort(key = lambda rec: rec.idNum)
+
+    # Print the student report.
+    printReport(studentList)
+
+def printReport(theList):
+    """
+    # Print the student report.
+    :param theList:
+    :return:
+    """
+    # The class names associated with the class codes.
+    classNames = (None, "Freshman", "Sophomore", "Junior", "Senior")
+
+    # Print the header.
+    print("LIST OF STUDENTS".center(50))
+    print("")
+    print("%5s %25s %10s %4s" % ('-'*5, '-'*25, '-'*10, '-'*4))
+
+    # Print the body.
+    for record in theList:
+        print("%5d %-25s %-10s %4.2f" % \
+              (record.idNum, record.lastName + ', ' + record.firstName, \
+               classNames[record.classCode], record.gpa))
+
+    # Add a footer.
+    print("-" * 50)
+    print("Number of students:", len(theList))
+
+main()
