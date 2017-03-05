7e03007fdb3457d2de2e8a6d515b74f252768cf9
Yulin Zhu <zieft@hotmail.com>
2017-03-05 12:06:40 +0100

    add binarysearch to searching.py

--- Searching/searching_algorithm.py
+++ Searching/searching_algorithm.py
@@ -1,4 +1,4 @@
-def linearSearch(the_values, target):
+def linear_search(the_values, target):
     """
     sequential search algorithm for unsorted sequence.
     :param the_values:
@@ -13,7 +13,8 @@ def linearSearch(the_values, target):
 
     return False
 
-def sortedLinearSearch(the_values, item):
+
+def sorted_linear_search(the_values, item):
     """
     sequential search algorithm for sorted sequence.
     :param the_values:
@@ -29,16 +30,46 @@ def sortedLinearSearch(the_values, item):
 
     return False
 
-def findSmallest(the_values):
+
+def find_smallest(the_values):
     """
     find the smallest num in a unsorted sequence.
     :param the_values:
     :return:
     """
-    n = len(the_values)
     smallest = the_values[0]
     for i in the_values:
         if i < smallest:
             smallest = i
     return smallest
 
+
+def binary_search(the_values, target):
+    """
+    Implementation of the binary search algorithm.
+    The variables low and high are used to mark the range of elements in
+    the sequence currently under consideration.
+    :param the_values:
+    :param target:
+    :return:
+    """
+    # Start with the entire sequence of elements.
+    low = 0
+    high = len(the_values) - 1
+
+    # Repeatedly subdivide the sequence in half until the target is found.
+    while low <= high:
+        # Find the midpoint of the sequence.
+        mid = (high + low) // 2
+        # Does the midpoint contain the target?
+        if the_values[mid] == target:
+            return True
+        # Or does the target precede the midpoint?
+        elif target < the_values[mid]:
+            high = mid - 1
+        # Or does it follow the midpoint?
+        else:
+            low = mid + 1
+
+    # If the sequence cannot be subdivided further, we're done.
+    return False
