c6c0da250a2d04f5e92ac25ce5cae0c60a3d74ee
Yulin Zhu <zieft@hotmail.com>
2017-04-04 16:27:54 +0200

    add _BinTreeNode class

---
+++ BineryTree/BinNode.py
@@ -0,0 +1,9 @@
+class _BinTreeNode:
+    """
+    The storage class for creating binary tree nodes.
+    """
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
